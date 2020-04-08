c80487aa63f234111e54d6e2c00ea163d40001ca
brendon cecilio <brendon.cecilio@gmail.com>
2020-04-08 18:41:13 -0400

    created User modal

---
+++ KVO-Lab/Modals/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  KVO-Lab
+//
+//  Created by Brendon Cecilio on 4/8/20.
+//  Copyright © 2020 Brendon Cecilio. All rights reserved.
+//
+
+import Foundation
+
+struct User {
+    let userName: String
+    let accountBalance: Double
+}
